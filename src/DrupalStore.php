149fb0d985dac13d71d5de92742fead25e4a6a31
Andrew Berry <deviantintegral@gmail.com>
2018-04-02 16:13:14 -0400

    Testing the DrupalStore

--- src/DrupalStore.php
+++ src/DrupalStore.php
@@ -8,73 +8,72 @@ use Symfony\Component\Lock\Exception\NotSupportedException;
 use Symfony\Component\Lock\Key;
 use Symfony\Component\Lock\StoreInterface;
 
+/**
+ *
+ */
 class DrupalStore implements StoreInterface {
 
   /**
+   * The Drupal lock backend.
+   *
    * @var \Drupal\Core\Lock\LockBackendInterface
    */
   private $lockBackend;
 
+  /**
+   *
+   */
   public function __construct(LockBackendInterface $lockBackend) {
     $this->lockBackend = $lockBackend;
   }
 
   /**
-   * Stores the resource if it's not locked by someone else.
-   *
-   * @throws \Symfony\Component\Lock\Exception\LockConflictedException
+   * {@inheritdoc}
    */
   public function save(Key $key) {
     $this->lock($key);
   }
 
   /**
-   * Waits until a key becomes free, then stores the resource.
-   *
-   * If the store does not support this feature it should throw a NotSupportedException.
-   *
-   * @throws \Symfony\Component\Lock\Exception\LockConflictedException
-   * @throws \Symfony\Component\Lock\Exception\NotSupportedException
+   * {@inheritdoc}
    */
   public function waitAndSave(Key $key) {
     $this->lock($key, TRUE);
   }
 
   /**
-   * Extends the ttl of a resource.
-   *
-   * If the store does not support this feature it should throw a NotSupportedException.
-   *
-   * @param float $ttl amount of second to keep the lock in the store
-   *
-   * @throws \Symfony\Component\Lock\Exception\LockConflictedException
-   * @throws \Symfony\Component\Lock\Exception\NotSupportedException
+   * {@inheritdoc}
    */
   public function putOffExpiration(Key $key, $ttl) {
     throw new NotSupportedException('Drupal locks can not have their expiration extended.');
   }
 
   /**
-   * Removes a resource from the storage.
+   * {@inheritdoc}
    */
   public function delete(Key $key) {
     $this->lockBackend->release($key);
   }
 
   /**
-   * Returns whether or not the resource exists in the storage.
-   *
-   * @return bool
+   * {@inheritdoc}
    */
   public function exists(Key $key) {
     return !$this->lockBackend->lockMayBeAvailable($key);
   }
 
   /**
+   * Try to acquire a lock.
+   *
    * @param \Symfony\Component\Lock\Key $key
+   *   The key to lock.
    * @param bool $blocking
+   *   (optional) TRUE if acquiring should wait for a lock, FALSE otherwise.
+   *
+   * @throws \Symfony\Component\Lock\Exception\LockConflictedException
+   *   Thrown if a lock could not be acquired.
    */
-  private function lock(Key $key, bool $blocking = false): bool {
+  private function lock(Key $key, bool $blocking = FALSE) {
     $duration = ($blocking ? 30 : 0);
     if (!$acquired = $this->lockBackend->acquire($key, $duration)) {
       if ($this->lockBackend->wait($key, $duration)) {
@@ -85,8 +84,6 @@ class DrupalStore implements StoreInterface {
     if (!$acquired) {
       throw new LockConflictedException(sprintf('Unable to acquire a lock for %s.', $key));
     }
-
-    return true;
   }
 
 }
---
+++ tests/src/Unit/DrupalStoreTest.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Lullabot\DrupalSymfonyLock\Test;
+
+use Drupal\Core\Lock\LockBackendInterface;
+use Lullabot\DrupalSymfonyLock\DrupalStore;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Lock\Exception\LockConflictedException;
+use Symfony\Component\Lock\Exception\NotSupportedException;
+use Symfony\Component\Lock\Key;
+
+/**
+ * @covers \Lullabot\DrupalSymfonyLock\DrupalStore
+ */
+class DrupalStoreTest extends TestCase {
+
+  /**
+   * The mock Drupal lock backend.
+   *
+   * @var \Drupal\Core\Lock\LockBackendInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $backend;
+
+  /**
+   * The store under test.
+   *
+   * @var \Lullabot\DrupalSymfonyLock\DrupalStore
+   */
+  protected $store;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->backend = $this->getMockBuilder(LockBackendInterface::class)
+      ->getMock();
+    $this->store = new DrupalStore($this->backend);
+  }
+
+  /**
+   * Tests saving a lock.
+   */
+  public function testSave() {
+    $this->backend->expects($this->once())->method('acquire')
+      ->with('test-key', 0)
+      ->willReturn(TRUE);
+
+    $key = new Key('test-key');
+    $this->store->save($key);
+  }
+
+  /**
+   * Tests a blocking save.
+   */
+  public function testWaitAndSave() {
+    $this->backend->expects($this->once())->method('acquire')
+      ->with('test-key', 30)
+      ->willReturn(TRUE);
+
+    $key = new Key('test-key');
+    $this->store->waitAndSave($key);
+  }
+
+  /**
+   * Tests retrying and succeeding a lock.
+   *
+   * @dataProvider retryDataProvider
+   *
+   * @param string $method
+   *   The method name to call.
+   * @param int $duration
+   *   The expected duration of the lock.
+   */
+  public function testRetry($method, $duration) {
+    $this->backend->expects($this->at(0))->method('acquire')
+      ->with('test-key', $duration)
+      ->willReturn(FALSE);
+
+    $this->backend->expects($this->at(1))->method('wait')
+      ->with('test-key', $duration)
+      ->willReturn(TRUE);
+
+    $this->backend->expects($this->at(2))->method('acquire')
+      ->with('test-key', $duration)
+      ->willReturn(TRUE);
+
+    $key = new Key('test-key');
+    $this->store->$method($key);
+  }
+
+  /**
+   * Methods to test for retries.
+   *
+   * @dataProvider
+   *
+   * @return array
+   *   An array with method and duration values.
+   */
+  public function retryDataProvider() {
+    return [
+      ['save', 0],
+      ['waitAndSave', 30],
+    ];
+  }
+
+  /**
+   * Tests when the wait() call fails.
+   */
+  public function testLockWaitFailed() {
+    $this->backend->expects($this->at(0))->method('acquire')
+      ->with('test-key', 0)
+      ->willReturn(FALSE);
+
+    $this->backend->expects($this->at(1))->method('wait')
+      ->with('test-key', 0)
+      ->willReturn(FALSE);
+
+    $key = new Key('test-key');
+    $this->expectException(LockConflictedException::class);
+    $this->store->save($key);
+  }
+
+  /**
+   * Tests when a second acquire() fails.
+   */
+  public function testLockAcquireFailed() {
+    $this->backend->expects($this->at(0))->method('acquire')
+      ->with('test-key', 0)
+      ->willReturn(FALSE);
+
+    $this->backend->expects($this->at(1))->method('wait')
+      ->with('test-key', 0)
+      ->willReturn(TRUE);
+
+    $this->backend->expects($this->at(2))->method('acquire')
+      ->with('test-key', 0)
+      ->willReturn(FALSE);
+
+    $key = new Key('test-key');
+    $this->expectException(LockConflictedException::class);
+    $this->store->save($key);
+  }
+
+  /**
+   * Test that we do not support extending expiration TTLs.
+   */
+  public function testPutOffExpiration() {
+    $this->expectException(NotSupportedException::class);
+    $this->store->putOffExpiration(new Key('resource'), 10);
+  }
+
+  /**
+   * Test releasing a lock.
+   */
+  public function testDelete() {
+    $this->backend->expects($this->once())->method('release')
+      ->with('test-key')
+      ->willReturn(TRUE);
+
+    $key = new Key('test-key');
+    $this->store->delete($key);
+  }
+
+  /**
+   * Tests checking if a lock exists.
+   */
+  public function testExists() {
+    $this->backend->expects($this->once())->method('lockMayBeAvailable')
+      ->with('test-key')
+      ->willReturn(TRUE);
+
+    $key = new Key('test-key');
+    $this->assertFalse($this->store->exists($key));
+  }
+
+}
